f89d8e9811c3f3877887355e3313048e490f0b67
Chris Novakovic <c.novakovic@cs.bham.ac.uk>
2018-12-17 11:51:20 +0000

    Expose state sorting option in PRISM's core functionality
    Although it is possible to enable or disable state sorting in an
    explicit ModelGenerator directly, it is not possible to do so via the
    prism.Prism class, which is the more common way to interact with the
    PRISM API. Create a new setting PRISM_SORT_STATES to control this
    behaviour in underlying explicit ModelGenerators, and add methods for
    controlling its value in prism.Prism.

--- prism/src/prism/Prism.java
+++ prism/src/prism/Prism.java
@@ -405,6 +405,11 @@ public class Prism extends PrismComponent implements PrismSettingsListener
 		settings.set(PrismSettings.PRISM_FIX_DEADLOCKS, b);
 	}
 
+	public void setSortStates(boolean b) throws PrismException
+	{
+		settings.set(PrismSettings.PRISM_SORT_STATES, b);
+	}
+
 	public void setDoProbChecks(boolean b) throws PrismException
 	{
 		settings.set(PrismSettings.PRISM_DO_PROB_CHECKS, b);
@@ -734,6 +739,11 @@ public class Prism extends PrismComponent implements PrismSettingsListener
 	public boolean getFixDeadlocks()
 	{
 		return settings.getBoolean(PrismSettings.PRISM_FIX_DEADLOCKS);
+	}
+
+	public boolean getSortStates()
+	{
+		return settings.getBoolean(PrismSettings.PRISM_SORT_STATES);
 	}
 
 	public boolean getDoProbChecks()
@@ -2102,6 +2112,7 @@ public class Prism extends PrismComponent implements PrismSettingsListener
 					}
 					ConstructModel constructModel = new ConstructModel(this);
 					constructModel.setFixDeadlocks(getFixDeadlocks());
+					constructModel.setSortStates(getSortStates());
 					currentModelExpl = constructModel.constructModel(currentModelGenerator);
 					currentModel = null;
 				}
@@ -2117,6 +2128,7 @@ public class Prism extends PrismComponent implements PrismSettingsListener
 				} else {
 					ConstructModel constructModel = new ConstructModel(this);
 					constructModel.setFixDeadlocks(getFixDeadlocks());
+					constructModel.setSortStates(getSortStates());
 					currentModelExpl = constructModel.constructModel(currentModelGenerator);
 					currentModel = null;
 				}
