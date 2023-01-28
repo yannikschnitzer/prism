fe67e56f040b06d5e3be0c1bd6b13c399dcb4106
IngyN <IngyN@users.noreply.github.com>
2023-01-28 16:52:52 -0500

    adjust for 170 4986

--- run_experiments.py
+++ run_experiments.py
@@ -353,7 +353,7 @@ config = {
     'grid_250_1500': {'model':prefix+'tests/gridmap/gridmap_250_1500.prism', 'props':prefix+'tests/gridmap/gridmap_250_1500.props', 'pn':[3,2], 'vmax': 900, 'epsilon':0.001, 'b':31, 'alpha':def_alpha},
     'grid_200_7140': {'model':prefix+'tests/gridmap/gridmap_200_7140.prism', 'props':prefix+'tests/gridmap/gridmap_200_7140.props', 'pn':[3,2], 'vmax': 800, 'epsilon':0.001, 'b':41, 'alpha':0.8},
     'grid_200_6776': {'model':prefix+'tests/gridmap/gridmap_200_6776.prism', 'props':prefix+'tests/gridmap/gridmap_200_6776.props', 'pn':[3,2], 'vmax': 800, 'epsilon':0.001, 'b':41, 'alpha':0.8},
-    'grid_170_4986': {'model':prefix+'tests/gridmap/gridmap_170_4986.prism', 'props':prefix+'tests/gridmap/gridmap_170_4986.props', 'pn':[3,2], 'vmax': 800, 'epsilon':0.001, 'b':101, 'alpha':0.8},
+    'grid_170_4986': {'model':prefix+'tests/gridmap/gridmap_170_4986.prism', 'props':prefix+'tests/gridmap/gridmap_170_4986.props', 'pn':[3,2], 'vmax': 600, 'epsilon':0.01, 'b':91, 'alpha':0.8},
     'grid_150_3918': {'model':prefix+'tests/gridmap/gridmap_150_3918.prism', 'props':prefix+'tests/gridmap/gridmap_150_3918.props', 'pn':[3,2], 'vmax': 800, 'epsilon':0.001, 'b':101, 'alpha':0.8},
 }
 
