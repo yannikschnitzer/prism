6dc47d55cd8667431ef1ec2d9932f722558dcb0a
IngyN <IngyN@users.noreply.github.com>
2023-11-01 10:37:03 -0400

    Quantile refactor done, missing distrbellmans

--- prism/src/explicit/DistributionQuantile.java
+++ prism/src/explicit/DistributionQuantile.java
@@ -39,16 +39,37 @@ import prism.PrismLog;
 
     // project a given array to finite support (same distribution parameters : vmin, vmiax support)
     // here arr is an array of the probability values for the same support
+    // If the support is the same (same number of atoms aka same probability)
+    // then we just need to make sure it is sorted.
     @Override
     public void project(ArrayList<Double> arr){
-
-
+        z =  new ArrayList<Double>(probs);
+        Collections.sort(z);
     }
 
     // project a given array to finite support (different distribution parameters but same number of atoms)
     @Override
-    public  void project(ArrayList<Double> probs, ArrayList<Double> supp, double vmin, double vmax){
-       
+    public void project(ArrayList<Double> probs, ArrayList<Double> supp, double vmin, double vmax){
+        double cum_p = 0.0;
+        ArrayList<MapEntry<Double, Double>> multimap = new ArrayList<>();
+        Map.Entry<Double, Double> entry;
+        for (int j = 0; j < atoms; j++) {
+            multimap.add(new MapEntry<>(probs.get(i), supp.get(i)));
+        }
+
+        multimap.sort(Map.Entry.comparingByValue());
+        z.clear();
+        Iterator<MapEntry<Double, Double>> it = multimap.iterator();
+
+        while(it.hasNext() & z.size() < atoms)
+        {
+            entry = it.next();
+            cum_p += entry.getKey();
+            if(cum_p >= tau_hat[z.size()]) {
+                z.add(entry.getValue());
+            }
+        }
+
     }
 
     // update saved distribution
@@ -61,53 +82,145 @@ import prism.PrismLog;
     @Override
     public double getExpValue()
     {
-       
+        double sum =0;
+        for (int j=0; j<atoms; j++)
+        {
+            sum+= p * z.get(j);
+        }
+        return sum;
     }
 
     @Override
     public double getExpValue(ArrayList<Double> temp)
     {
-        
+        double sum =0;
+        for (int j=0; j<atoms; j++)
+        {
+            sum+= p * temp.get(j);
+        }
+        return sum;
     }
 
     // compute CVaR with a given alph
     @Override
     public double getCvarValue(double alpha)
     {
-        
+        double res =0.0;
+        double sum_p =0.0;
+        double denom = 0.0;
+
+        // assume already sorted
+
+        for (int i=atoms-1; i>=0; i--){
+            if (sum_p < lim){
+                if(sum_p+ p < lim){
+                    sum_p += p;
+                    res += (1/lim) * z.get(i) * p;
+                } else{
+                    denom = lim - sum_p;
+                    sum_p += denom;
+                    res += (1/lim) *denom*z.get(i);
+                }
+            }
+        }
+
+        return res;
     }
 
     // compute CVaR with a given alpha 
     @Override
     public double getCvarValue(ArrayList<Double> probs, double alpha)
     {
-       
+        double res =0.0;
+        double sum_p =0.0;
+        double denom = 0.0;
+        ArrayList<Double> temp =  new ArrayList<Double>(probs);
+        Collections.sort(temp);
+        for (int i=atoms-1; i>=0; i--){
+            if (sum_p < lim){
+                if(sum_p+ p < lim){
+                    sum_p += p;
+                    res += (1/lim) * temp.get(i) * p;
+                } else{
+                    denom = lim - sum_p;
+                    sum_p += denom;
+                    res += (1/lim) *denom*temp.get(i);
+                }
+            }
+        }
+
+        return res;
     }
 
     // compute value at risk
     @Override
     public double getVar(double alpha)
     {
+        // Assume already sorted
+        double res =0.0;
+        double sum_p =0.0;
 
+        for(int j=atoms-1; j>=0; j--){
+            if (sum_p < lim){
+                if(sum_p+ p < lim){
+                    sum_p += p;
+                } else{
+                    res =z.get(j);
+                }
+            }
+        }
+
+        return res;
     }
 
     // compute value at risk
     @Override
     public double getVar(ArrayList<Double> probs, double alpha)
     {
+        double res =0.0;
+        double sum_p =0.0;
+        ArrayList<Double> temp =  new ArrayList<Double>(probs);
+        Collections.sort(temp);
 
+        for(int j=atoms-1; j>=0; j--){
+            if (sum_p < lim){
+                if(sum_p+ p < lim){
+                    sum_p += p;
+                } else{
+                    res =temp.get(j);
+                }
+            }
+        }
+
+        return res;
     }
 
     // compute variance
     @Override
     public double getVariance(){
+        double mu = getExpValue(z);
+        double res = 0.0;
 
+        for( int j = 0; j<atoms; j++)
+        {
+            res += (1.0 / atoms) * pow(((z.get(j) * p) - mu), 2);
+        }
+
+        return res;
     }
 
     // compute variance
     @Override
     public double getVariance(ArrayList<Double> probs){
+        double mu = getExpValue(probs);
+        double res = 0.0;
 
+        for( int j = 0; j<atoms; j++)
+        {
+            res += (1.0 / atoms) * pow(((probs.get(j) * p) - mu), 2);
+        }
+
+        return res;
     }
 
     // compute W for relevant p, categorical: p=2, quantile p=1
