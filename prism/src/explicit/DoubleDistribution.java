92db316a51a73e0364ec67e47e578fefb1482261
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2024-12-23 17:15:10 +0100

    Polytopic uncertainty sets

--- prism/src/explicit/DoubleDistribution.java
+++ prism/src/explicit/DoubleDistribution.java
@@ -1,5 +1,7 @@
 package explicit;
 
+import java.util.Map;
+
 public class DoubleDistribution
 {
     public int size;
@@ -31,5 +33,16 @@ public class DoubleDistribution
             s += "[" + probs[i] + "]:" + index[i];
         }
         return s;
+    }
+
+    public static DoubleDistribution extractDoubleDistribution(Distribution<Double> frequencies) {
+        DoubleDistribution dist = new DoubleDistribution(frequencies.size());
+        int i = 0;
+        for (Map.Entry<Integer, Double> entry : frequencies.map.entrySet()) {
+            dist.probs[i] = ((Map.Entry<Integer, Double>) entry).getValue();
+            dist.index[i] = entry.getKey();
+            i++;
+        }
+        return dist;
     }
 }
