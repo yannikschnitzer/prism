67ae732f802212e1255946f22e2f7052311ac237
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2024-12-28 15:57:32 +0100

    convex learner

--- prism/src/explicit/ConvexLearner.java
+++ prism/src/explicit/ConvexLearner.java
@@ -11,17 +11,12 @@ public class ConvexLearner {
 
     public static void main(String[] args) throws PrismException {
         UMDPModelChecker umdp = new UMDPModelChecker(null);
-        MDP<Function> mdp = new MDPSimple<>();
-        ConvexLearner cx = new ConvexLearner();
-        cx.initializePrism();
-    }
 
-    @SuppressWarnings("unchecked")
-    public void initializePrism() throws PrismException {
-        this.prism = new Prism(new PrismDevNullLog());
-        this.prism.initialise();
-        this.prism.setEngine(Prism.EXPLICIT);
-        this.prism.setGenStrat(true);
+        MDPSimple<Function> mdp = new MDPSimple<>();
+        mdp.addStates(4);
+
+
+
     }
 
 }
