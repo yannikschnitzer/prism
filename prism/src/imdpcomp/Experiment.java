24e3b24a8a37de014c4268e437f542538c109d99
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2025-06-01 14:05:12 +0100

    Update Experiments

--- prism/src/imdpcomp/Experiment.java
+++ prism/src/imdpcomp/Experiment.java
@@ -101,7 +101,7 @@ public class Experiment {
                 this.parameterValues.addValue("r", 0.80);
                 this.parameterValues.addValue("p",0.2);
                 this.parameterValues.addValue("maxX",10);
-                this.parameterValues.addValue("maxY",20);
+                this.parameterValues.addValue("maxY",15);
             }
 
             case AIRCRAFT_MULTI_SLIP -> {
