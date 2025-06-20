cce3381879df68b25060104e15b9562bdb1059c6
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2025-06-20 16:46:17 +0100

    Updated Aircraft benchmark

--- prism/src/imdpcomp/ExperimentRunner.java
+++ prism/src/imdpcomp/ExperimentRunner.java
@@ -188,7 +188,7 @@ public class ExperimentRunner implements Callable<Integer> {
 
     public static void mai3n(String[] args) {
         ExperimentRunner experimentRunner = new ExperimentRunner();
-        Experiment experiment = new Experiment(LAKE_SWARM_MULTI_SLIP);
+        Experiment experiment = new Experiment(AIRCRAFT_MULTI_SLIP);
 
         try {
             experimentRunner.runExperimentAllTypes(experiment);
